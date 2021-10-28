cdf8f46b77be316eafc3fdda3e4305e882e7dbbf
GregCls <Gcloiseau@gmail.com>
2021-10-28 11:46:47 +0200

    Bluetooth ON_OFF OK

--- app/src/main/java/com/example/irobot_app/MainActivity.java
+++ app/src/main/java/com/example/irobot_app/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.irobot_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Button DeployButton;
+    private ImageButton BluetoothButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DeployButton = (Button) findViewById(R.id.DeployButton);
+        BluetoothButton = (ImageButton) findViewById(R.id.BluetoothButton);
+
+
+        DeployButton.setOnClickListener(v -> {
+            openRobotCommand();
+        });
+
+        BluetoothButton.setOnClickListener(v -> {
+            openBluetoothMenu();
+        });
+    }
+
+    public void openRobotCommand(){
+        Intent intent = new Intent(this, RobotCommand.class);
+        startActivity(intent);
+    }
+
+    public void openBluetoothMenu(){
+        Intent intent = new Intent(this, BluetoothConnection.class);
+        startActivity(intent);
     }
 }
